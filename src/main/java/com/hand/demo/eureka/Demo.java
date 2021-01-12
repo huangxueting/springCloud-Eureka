0bedb9687e90c244c693a84a2a449dd51a0e62d9
29173 <xueting.huang@hand-china.com>
2021-01-12 14:13:33 +0800

    删除测试

--- src/main/java/com/hand/demo/eureka/Demo.java
+++ src/main/java/com/hand/demo/eureka/Demo.java
@@ -3,8 +3,5 @@ package com.hand.demo.eureka;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("args = " + args);
-        System.out.println("haha");
-        System.out.println("hehe");
-        System.out.println("hiahia");
     }
 }
