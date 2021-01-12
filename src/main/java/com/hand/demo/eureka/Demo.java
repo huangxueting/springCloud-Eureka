3c088fcc4cef46b7fe4a65e760975a3c5689ef0e
29173 <xueting.huang@hand-china.com>
2021-01-12 13:49:35 +0800

    修改测试1

--- src/main/java/com/hand/demo/eureka/Demo.java
+++ src/main/java/com/hand/demo/eureka/Demo.java
@@ -3,5 +3,6 @@ package com.hand.demo.eureka;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("args = " + args);
+        System.out.println("haha");
     }
 }
